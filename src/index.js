fe0d82bc444a648ee45459a1ff2a1867adaf54ef
tamdt5 <85442498+tamdt5@users.noreply.github.com>
2023-06-14 09:57:33 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-import Todorework from './Todorework';
+import Weather from './Weather';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Todorework />
+    <Weather />
   </React.StrictMode>
 );
 
